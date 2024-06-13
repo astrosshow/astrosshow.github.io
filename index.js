2822c1d1e9ba8163108dfcb4ce13fcd55889ed26
astrosshow <130030251+astrosshow@users.noreply.github.com>
2024-06-13 16:40:49 -0700

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+function setLanguage(language) {
+    if (language === 'en') {
+        alert('Language changed to English')
+    } else if (language === 'es') {
+        alert('Language changed to Spanish')
+    }
+}
